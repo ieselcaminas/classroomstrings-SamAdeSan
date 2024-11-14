6101c1758826b90ec2bc2c0a4b2d11df97786848
samadesan <samadesan@alu.edu.gva.es>
2024-11-14 12:59:28 +0100

    Parentesis.java

--- src/main/Parentesis.java
+++ src/main/Parentesis.java
@@ -1,2 +1,36 @@
 public class Parentesis {
+    public static boolean bienParentesis (String cadena) {
+        int contador = 0;
+        char caracter;
+        for (int i = 0; i < cadena.length(); i++) {
+            caracter = cadena.charAt(i);
+            if (caracter == '(') {
+                contador++;
+            }else if (caracter == ')') {
+                contador--;
+                if (contador < 0) {
+                    return true;
+                }
+            }
+        }
+        return contador == 0;
+    }
+    public static boolean siOnoParentizada (String cadena) {
+        if (bienParentesis(cadena)) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(bienParentesis("Esto(es) )una cadena)(parentizada)"));
+        String parentesis = "";
+        if (siOnoParentizada(parentesis)) {
+            System.out.println("Está bien parentizado.");
+        }
+        else {
+            System.out.println("Está mal parentizado.");
+        }
+
+    }
 }
